3876280621ad879cd8a6e5a6fddd662c340b40e9
윤병익 <by_yeun@daum.net>
2017-10-06 15:31:40 +0900

    some modifications to support recent versions of Rust

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,7 @@
 //  
 //  You should have received a copy of the GNU Affero General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#![feature(io, pub_restricted, associated_consts)]
+#![feature(io, pub_restricted)]
 
 extern crate base64;
 extern crate mime;
